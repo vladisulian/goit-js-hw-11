1e2a06e2a4f78126efd86e3047994eb0788d5eab
Vladislav Ulianeev <vladis.ulian@gmail.com>
2022-12-13 17:49:45 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import { fetchImages } from './fetchImages';
-import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
